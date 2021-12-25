24430a6ecb61e80db7efad7c38f270046c75eddf
dankornas <86244964+dankornas@users.noreply.github.com>
2021-12-25 15:03:42 +0100

    created twitterscraper script

---
+++ twitterscraper.py
@@ -0,0 +1,171 @@
+import csv
+from time import sleep
+from msedge.selenium_tools import Edge, EdgeOptions
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.common import exceptions
+from time import sleep
+from datetime import date
+
+def create_webdriver_instance():
+    options = EdgeOptions()
+    options.use_chromium = True
+    driver = Edge(options=options)
+    return driver
+
+
+def get_joined_date(driver, twitterHandle):
+    driver.get('https://twitter.com/'+twitterHandle)
+    sleep(3)
+    header = driver.find_elements_by_xpath('//div[@data-testid="UserProfileHeader_Items"]')
+    joined_date = header[0].text.split('Joined ')[-1]
+    return joined_date
+
+def get_month_num(month):
+    monthNum = {'January': 1, 
+                'February': 2,
+                'March': 3,
+                'April': 4,
+                'May': 5,
+                'June': 6,
+                'July': 7,
+                'August': 8,
+                'September': 9,
+                'October': 10,
+                'November': 11,
+                'December': 12}
+
+    return(monthNum[month])
+
+def generate_tweet_id(tweet):
+    return ''.join(tweet)
+
+
+def scroll_down_page(driver, last_position, num_seconds_to_load=0.5, scroll_attempt=0, max_attempts=5):
+    """The function will try to scroll down the page and will check the current
+    and last positions as an indicator. If the current and last positions are the same after `max_attempts`
+    the assumption is that the end of the scroll region has been reached and the `end_of_scroll_region`
+    flag will be returned as `True`"""
+    end_of_scroll_region = False
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    sleep(num_seconds_to_load)
+    curr_position = driver.execute_script("return window.pageYOffset;")
+    if curr_position == last_position:
+        if scroll_attempt < max_attempts:
+            end_of_scroll_region = True
+        else:
+            scroll_down_page(last_position, curr_position, scroll_attempt + 1)
+    last_position = curr_position
+    return last_position, end_of_scroll_region
+
+
+def save_tweet_data_to_csv(records, filepath, mode='a+'):
+    header = ['User', 'Handle', 'PostDate', 'TweetText', 'ReplyCount', 'RetweetCount', 'LikeCount']
+    with open(filepath, mode=mode, newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        if mode == 'w':
+            writer.writerow(header)
+        if records:
+            writer.writerow(records)
+
+
+def collect_all_tweets_from_current_view(driver, lookback_limit=25):
+    page_cards = driver.find_elements_by_xpath('//article[@data-testid="tweet"]')
+    if len(page_cards) <= lookback_limit:
+        return page_cards
+    else:
+        return page_cards[-lookback_limit:]
+
+
+def extract_data_from_current_tweet_card(card):
+    try:
+        user = card.find_element_by_xpath('.//span').text
+    except exceptions.NoSuchElementException:
+        user = ""
+    except exceptions.StaleElementReferenceException:
+        return
+    try:
+        handle = card.find_element_by_xpath('.//span[contains(text(), "@")]').text
+    except exceptions.NoSuchElementException:
+        handle = ""
+    try:
+        postdate = card.find_element_by_xpath('.//time').get_attribute('datetime')
+    except exceptions.NoSuchElementException:
+        return
+    try:
+        _comment = card.find_element_by_xpath('.//div[2]/div[2]/div[2]/div[1]').text
+    except exceptions.NoSuchElementException:
+        _comment = ""
+    try:
+        _responding = card.find_element_by_xpath('.//div[2]/div[2]/div[2]/div[2]').text
+    except exceptions.NoSuchElementException:
+        _responding = ""
+    tweet_text = _comment + _responding
+    try:
+        reply_count = card.find_element_by_xpath('.//div[@data-testid="reply"]').text
+    except exceptions.NoSuchElementException:
+        reply_count = ""
+    try:
+        retweet_count = card.find_element_by_xpath('.//div[@data-testid="retweet"]').text
+    except exceptions.NoSuchElementException:
+        retweet_count = ""
+    try:
+        like_count = card.find_element_by_xpath('.//div[@data-testid="like"]').text
+    except exceptions.NoSuchElementException:
+        like_count = ""
+
+    tweet = (user, handle, postdate, tweet_text, reply_count, retweet_count, like_count)
+    return tweet
+
+
+def scrapeProfile(twitterHandle, filepath, page_sort='Latest'):
+    save_tweet_data_to_csv(None, filepath, 'w')  # create file for saving records
+    last_position = None
+    end_of_scroll_region = False
+    unique_tweets = set()
+
+    driver = create_webdriver_instance()
+    
+    joined_date = get_joined_date(driver, twitterHandle)
+    joinedMonthNum = get_month_num(joined_date.split()[0])
+    joinedYearNum = joined_date.split()[1]
+
+    sleep(2)
+
+    searchQuery = 'https://twitter.com/search?q=(from%3A'+twitterHandle+')%20until%3A'+str(date.today())+'%20since%3A'+str(joinedYearNum)+'-'+str(joinedMonthNum)+'-01%20-filter%3Areplies&src=typed_query&f=live'
+
+    driver.get(searchQuery)
+    sleep(2)
+    # search_found = find_search_input_and_enter_criteria(search_term, driver)
+    # if not search_found:
+    #     return
+
+    # change_page_sort(page_sort, driver)
+
+    while not end_of_scroll_region:
+        cards = collect_all_tweets_from_current_view(driver)
+        print("Collected Cards")
+        for card in cards:
+            try:
+                tweet = extract_data_from_current_tweet_card(card)
+            except exceptions.StaleElementReferenceException:
+                continue
+            if not tweet:
+                continue
+            tweet_id = generate_tweet_id(tweet)
+            if tweet_id not in unique_tweets:
+                unique_tweets.add(tweet_id)
+                save_tweet_data_to_csv(tweet, filepath)
+        last_position, end_of_scroll_region = scroll_down_page(driver, last_position)
+        sleep(1)
+    print("Finished")
+    driver.quit()
+
+
+if __name__ == '__main__':
+    twitterUser = 'dankornas'
+    path = twitterUser+'.csv'
+
+    main(twitterUser, path)
